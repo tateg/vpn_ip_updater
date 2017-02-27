60ea9a6a81abc5ba636e1f5e9abfedf5742dc626
Tate Galbraith <tate.galbraith@gmail.com>
2017-02-27 14:48:39 -0800

    Basic layout, class and methods

--- lib/vpn_ip_updater.rb
+++ lib/vpn_ip_updater.rb
@@ -2,3 +2,44 @@
 # Written by Tate Galbraith
 # Feb 2017
 
+require 'open-uri'
+require 'dashboard-api'
+
+class VPNIPUpdater
+	
+	# Get current public IP of this machine against Akamai
+	def get_current_ip
+		@scrape = "http://whatismyip.akamai.com"
+		@ip = open(@scrape).read
+	end
+
+	# Instantiate new API instance using API key passed in as argument
+	def connect_api
+		@api_key = ARGV.first
+		@dash_api = DashboardAPI.new(@api_key)
+	end
+
+	# Get current list of organization third-party peers
+	def get_peer_list
+		@org_id = ARGV.second
+		@peers = @dash_api.get_third_party_peers(@org_id)
+	end
+
+	# Filter peer list for specified peer and get current IP for that peer
+	def get_peer_current_ip
+		@peers.each do |list|
+			list.each do |k, v|
+				if :name == ARGV.third
+					@current_peer_ip = :publicIp
+				end
+			end
+		end
+		puts @current_peer_ip
+	end
+
+end
+
+VPNIPUpdater.get_current_ip
+VPNIPUpdater.connect_api
+VPNIPUpdater.get_peer_list
+VPNIPUpdater.get_peer_current_ip
