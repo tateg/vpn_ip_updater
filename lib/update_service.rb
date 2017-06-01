aa99d1987430f1940edd23401c2e09a2713362bb
Tate Galbraith <tate.galbraith@gmail.com>
2017-06-01 09:22:30 -0700

    Updated service help text and added interval call

--- lib/update_service.rb
+++ lib/update_service.rb
@@ -15,10 +15,18 @@ options = {}
 
 OptionParser.new do |parser|
 
-  parser.separator "----------------------"
-  parser.separator "VPN IP Updater Service"
-  parser.separator "----------------------"
-  parser.banner = "Usage: update_service.rb [options]"
+  parser.banner = "
+    ----------------------------------
+    VPN IP Updater Service
+    ----------------------------------
+
+    Usage: update_service.rb [options]
+    + If nothing is provided this help will be shown
+    + If only API key is provided then a list of all associated organizations will be returned
+    + If API key and org ID is provided a list of all third-party VPN peers will be returned
+    + If API key, org ID and peer name is provided that peer public IP will be updated
+    + If API key, org ID, peer name and interval is provided that peer will be updated on interval"
+  parser.separator ""
 
   parser.on("-h", "--help", "Show this help message") do ||
     puts parser
@@ -32,10 +40,11 @@ OptionParser.new do |parser|
   parser.on("-p", "--peer NAME", "VPN peer name to update") do |p|
     options[:peer] = p
   end
-  parser.on("-i", "--update-interval SECONDS", "Number of seconds between IP checks") do |i|
+  parser.on("-i", "--update-interval SECONDS", Integer, "Number of seconds between IP checks") do |i|
     options[:interval] = i
   end
 
+  parser.separator ""
 end.parse!
 
 # Instantiate updater if API key present
@@ -53,8 +62,18 @@ if options.key?(:key) and options.key?(:organization) and !options.key?(:peer)
   puts updater.get_vpn_peers(options[:organization])
 end
 
-# Update peer if all three arguments are provided
-if ([:key, :organization, :peer].all? {|k| options.key? k})
+# Update peer one time if all three arguments except interval are provided
+if ([:key, :organization, :peer].all? {|k| options.key? k}) and !options.key?(:interval)
   puts "Updating named VPN peer public IP".yellow
   updater.update_vpn_peer_ip(options[:organization], options[:peer])
 end
+
+# Update peer on loop at interval if all four arguments provided
+if ([:key, :organization, :peer, :interval].all? {|k| options.key? k})
+  loop do
+    puts "Updating named VPN peer public IP on #{options[:interval]} second interval".yellow
+    updater.update_vpn_peer_ip(options[:organization], options[:peer])
+    sleep options[:interval]
+  end
+end
+
